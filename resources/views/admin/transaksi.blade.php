d4bbafe19f1b1323931903b95ebaa76590361fe1
Gamdara <poiu12345654321@gmail.com>
2019-02-21 12:19:46 +0800

    new admin

--- resources/views/admin/transaksi.blade.php
+++ resources/views/admin/transaksi.blade.php
@@ -1,59 +1,59 @@
 @extends('layouts.admin')
 @section('content')
 <div class="row mb-4">
-        <div class="col-md">
-            <div class="d-flex border">
-                <div class="bg-primary text-light p-4">
-                    <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-tasks"></i>
-                    </div>
-                </div>
-                <div class="flex-grow-1 bg-white p-4">
-                    <p class="text-uppercase text-secondary mb-0">Unverified</p>
-                    <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','unverified')->count()}}</h3>
+    <div class="col-md">
+        <div class="d-flex border">
+            <div class="bg-primary text-light p-4">
+                <div class="d-flex align-items-center h-100">
+                    <i class="fa fa-3x fa-fw fa-tasks"></i>
                 </div>
             </div>
-        </div>
-        <div class="col-md">
-            <div class="d-flex border">
-                <div class="bg-success text-light p-4">
-                    <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-check"></i>
-                    </div>
-                </div>
-                <div class="flex-grow-1 bg-white p-4">
-                    <p class="text-uppercase text-secondary mb-0">Success</p>
-                    <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','success')->count()}}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-md">
-            <div class="d-flex border">
-                <div class="bg-danger text-light p-4">
-                    <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-ban"></i>
-                    </div>
-                </div>
-                <div class="flex-grow-1 bg-white p-4">
-                    <p class="text-uppercase text-secondary mb-0">Cancelled</p>
-                    <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','cancelled')->count()}}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-md">
-            <div class="d-flex border">
-                <div class="bg-info text-light p-4">
-                    <div class="d-flex align-items-center h-100">
-                        <i class="fa fa-3x fa-fw fa-exclamation-triangle "></i>
-                    </div>
-                </div>
-                <div class="flex-grow-1 bg-white p-4">
-                    <p class="text-uppercase text-secondary mb-0">Expired</p>
-                    <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','expired')->count()}}</h3>
-                </div>
+            <div class="flex-grow-1 bg-white p-4">
+                <p class="text-uppercase text-secondary mb-0">Unverified</p>
+                <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','unverified')->count()}}</h3>
             </div>
         </div>
     </div>
+    <div class="col-md">
+        <div class="d-flex border">
+            <div class="bg-success text-light p-4">
+                <div class="d-flex align-items-center h-100">
+                    <i class="fa fa-3x fa-fw fa-check"></i>
+                </div>
+            </div>
+            <div class="flex-grow-1 bg-white p-4">
+                <p class="text-uppercase text-secondary mb-0">Success</p>
+                <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','success')->count()}}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="d-flex border">
+            <div class="bg-danger text-light p-4">
+                <div class="d-flex align-items-center h-100">
+                    <i class="fa fa-3x fa-fw fa-ban"></i>
+                </div>
+            </div>
+            <div class="flex-grow-1 bg-white p-4">
+                <p class="text-uppercase text-secondary mb-0">Cancelled</p>
+                <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','cancelled')->count()}}</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="d-flex border">
+            <div class="bg-info text-light p-4">
+                <div class="d-flex align-items-center h-100">
+                    <i class="fa fa-3x fa-fw fa-exclamation-triangle "></i>
+                </div>
+            </div>
+            <div class="flex-grow-1 bg-white p-4">
+                <p class="text-uppercase text-secondary mb-0">Expired</p>
+                <h3 class="font-weight-bold mb-0">{{$transaksis->where('status','expired')->count()}}</h3>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="card mb-4">
     <div class="card-body">
@@ -90,11 +90,10 @@
     </div>
 </div>
 
-
-    <script>
-        $(document).ready(function () {
-            $('#example').DataTable();
-        });
-    </script>
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+</script>
 
 @endsection
