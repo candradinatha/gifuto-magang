aa0121606cb3031751f11b5fd7ffbee117d55775
Gamdara <poiu12345654321@gmail.com>
2019-03-22 15:02:49 +0800

    v2

--- resources/views/admin/status_seller.blade.php
+++ resources/views/admin/status_seller.blade.php
@@ -6,7 +6,7 @@
         <div class="d-flex border">
             <div class="bg-primary text-light p-4">
                 <div class="d-flex align-items-center h-100">
-                    <i class="fa fa-3x fa-fw fa-cog"></i>
+                    <i class="fa fa-3x fa-fw fa-user"></i>
                 </div>
             </div>
             <div class="flex-grow-1 bg-white p-4">
@@ -19,9 +19,9 @@
 <div class="row mb-4"> -->
     <div class="col-md">
         <div class="d-flex border">
-            <div class="bg-primary text-light p-4">
+            <div class="bg-danger text-light p-4">
                 <div class="d-flex align-items-center h-100">
-                    <i class="fa fa-3x fa-fw fa-cog"></i>
+                    <i class="fa fa-3x fa-fw fa-user-times"></i>
                 </div>
             </div>
             <div class="flex-grow-1 bg-white p-4">
